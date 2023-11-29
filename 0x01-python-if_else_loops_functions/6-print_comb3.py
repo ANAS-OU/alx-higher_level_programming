93019d8af3dd5535697395e72f17db0bdc515b03
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-11-30 00:19:59 +0100

    update for pycodestyle

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,7 @@
 for i in range(0, 10):
     for j in range(i + 1, 10):
         print("{:d}{:d}".format(i, j), end="")
-        if i != 8: print(end=", ")
-        else: print()
+        if i != 8:
+            print(end=", ")
+        else:
+            print()
