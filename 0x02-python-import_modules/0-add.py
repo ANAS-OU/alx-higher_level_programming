3a5d48ca98be8f6146dd875e135435c0b6daac56
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-12-01 02:51:35 +0100

    0. Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
+import add_0 as my_module
 a = 1
 b = 2
-rst = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, rst))
+rst = my_module.add(a, b)
+print("{} + {} = {}".format(a, b, rst))
