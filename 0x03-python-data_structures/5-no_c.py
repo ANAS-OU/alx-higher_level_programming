9f84c1c1c083d33460bd18c56b8227ea8efee9a9
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-12-06 00:05:50 +0100

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def no_c(my_string):
     my_list = list(my_string)
-    for c in ['c', 'C']:
-        if c in my_list:
+    i = 0
+    while i < len(my_list):
+        c = my_list[i]
+        if c in ['c', 'C']:
             my_list.remove(c)
+        else:
+            i += 1
     my_string = ''.join(my_list)
     return (my_string)
