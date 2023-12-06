b29ab1fd8039f0bdfe48aa262ba284410c37e677
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-12-06 21:59:15 +0100

    0. Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    if not matrix:
+        return
+
     new_matrix = []
     for arr in matrix:
         new_matrix += list(map(lambda x: x ** 2, arr))
