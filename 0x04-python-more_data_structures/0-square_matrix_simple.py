4a45f14edc0c2a645cb2a66aa889f01ee64a5ec4
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-12-06 21:55:30 +0100

    0. Squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for arr in matrix:
+        new_matrix += list(map(lambda x: x ** 2, arr))
+    return (new_matrix)
