4edcf79495b37fb35d4fdba9626f5bedee27590c
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-12-07 02:44:30 +0100

    1. Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,5 +4,8 @@ def search_replace(my_list, search, replace):
     search_replace - a function that replaces all occurrences
     of an element by another in a new list
     """
+    if not my_list:
+        return
+
     new_list = list(map(lambda x: replace if x == search else x, my_list))
     return (new_list)
