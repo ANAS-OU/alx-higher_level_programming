444949ed27eb872aea3aac79e9dd7a0fff4a51a6
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-12-18 20:20:04 +0100

    0. Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,9 @@
 #!/bin/python3
 def safe_print_list(my_list=[], x=0):
+    """
+    safe_print_list - a function that prints <x>
+    element of <my_list>
+    """
     try:
         for i in range(x):
             print(my_list[i], end="")
