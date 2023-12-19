1f0d627cc05eb5109f02b138af24f7e7de13d25e
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-12-20 00:40:40 +0100

    1. Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+
+"""
+Writing an empty class Square
+that defines a square
+
+"""
+
+
 class Square:
+    """ Class Attributes """
     __size = 0
 
     def __init__(self, size=0):
+        """ Instantiation """
         if size:
             self.__size = size
