b79edc04b86bbad7dd32b754b57a298975868f19
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-12-20 00:57:17 +0100

    2. Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+"""
+Defining a class Square
+
+"""
+
+
+class Square:
+
+    """ Class Attributes """
+    __size = 0
+
+    def __init__(self, size=0):
+
+        """ Instantiation """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
