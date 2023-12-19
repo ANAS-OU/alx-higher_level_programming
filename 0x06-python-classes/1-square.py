92d753e246fb35f26ba3856da7f16d8449298db2
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-12-20 00:21:26 +0100

    1. Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class Square:
-    __size = None
+    __size = 0
 
-    def __init__(self, new_size=None):
-        if new_size is not None:
-            self.__size = new_size
+    def __init__(self, size=0):
+        if size:
+            self.__size = size
