e8e481bc8673c097b2094053b0f664af8ea5d994
anas-ou <anas.outiskte@edu.uiz.ac.ma>
2024-02-13 02:52:29 +0100

    update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,6 +14,7 @@ class Rectangle:
     """Defines a rectange"""    
 
     def __init__(self, width=0, height=0):
+        """Initializes a Rectangle object with the given width and height."""
         self.width = width
         self.height = height
 
