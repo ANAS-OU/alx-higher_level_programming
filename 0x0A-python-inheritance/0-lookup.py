decc56c3d64ae9b42707f12ffac68fe924b45499
anas-ou <anas.outiskte@edu.uiz.ac.ma>
2024-02-19 16:51:27 +0100

    0. Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Inheritance Module
+
+The purpose of that module is to play around,
+and practice the Inheritance Principle in Python.
+"""
+
+
+def lookup(obj):
+    """
+    Returns List of that object's
+    methods and attributes
+    """
+    return dir(obj)
