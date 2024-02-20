b4eb562f36c8d29c1b777b9298e8b171d64de9e6
anas-ou <anas.outiskte@edu.uiz.ac.ma>
2024-02-20 03:42:11 +0100

    3. Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Inheritance Module
+
+The purpose of that module is to play around,
+and practice the Inheritance Principle in Python.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check if an object is an instance
+    of the given class"""
+    return isinstance(obj, a_class)
