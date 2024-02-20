84217dc4cf6213d9c19d1bd5f35f3b0bb30e3e5e
anas-ou <anas.outiskte@edu.uiz.ac.ma>
2024-02-20 14:43:59 +0100

    9. Full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Inheritance Module
+
+The purpose of that module is to play around, and
+practice the Inheritance Principle in Python.
+"""
+
+
+class BaseGeometry:
+    """Geometry Class"""
+    def area(self):
+        """Area Method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method that validate <value> argument"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle Class"""
+    def __init__(self, width, height):
+        """Initialization"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Method computes the rectangle area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """rectange description"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
