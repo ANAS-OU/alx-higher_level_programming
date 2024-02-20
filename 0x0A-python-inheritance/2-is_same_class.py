5e1f69108a3d8d41a5c5479829a99049d705f0d5
anas-ou <anas.outiskte@edu.uiz.ac.ma>
2024-02-20 03:30:06 +0100

    2. Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Inheritance Module
+
+The purpose of that module is to play around,
+and practice the Inheritance Principle in Python.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    function checks if an object is an
+    instance of a given class
+    """
+    return type(obj) is a_class
