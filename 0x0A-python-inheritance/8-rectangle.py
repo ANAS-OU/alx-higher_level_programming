d5c6aa18762c7696dd9e0da7e513545705f6158c
anas-ou <anas.outiskte@edu.uiz.ac.ma>
2024-02-20 15:09:24 +0100

    update

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,20 +7,6 @@ practice the Inheritance Principle in Python.
 """
 
 
-class BaseGeometry:
-    """Geometry Class"""
-    def area(self):
-        """Area Method"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Method that validate <value> argument"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
 class Rectangle(BaseGeometry):
     """Rectangle Class"""
     def __init__(self, width, height):
