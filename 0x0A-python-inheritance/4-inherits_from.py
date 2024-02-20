76e7f75a48e16d4e57d5beb17a7b076004ef92af
anas-ou <anas.outiskte@edu.uiz.ac.ma>
2024-02-20 03:59:30 +0100

    3. Same class or inherit from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Inheritance Module
+
+The purpose of that module is to play around,
+and practice the Inheritance Principle in Python.
+"""
+
+
+def inherits_from(obj, a_class):
+    """check if an object inherited from
+    the given class"""
+    if type(obj) is a_class:
+        return False
+    return issubclass(type(obj), a_class)
