72908d8cde6f54b4ca90777ea2d2d3c7bc9530d6
anas-ou <anas.outiskte@edu.uiz.ac.ma>
2024-02-20 13:56:52 +0100

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Inheritance Module
+
+The purpose of that module is to play around, and
+practice the Inheritance Principle in Python.
+"""
+
+
+class BaseGeometry:
+    """Geometry Class"""
+    def area(self):
+        """Area Method"""
+        raise Exception("area() is not implemented")
