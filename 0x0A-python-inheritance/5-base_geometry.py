dfa67e6e67aa3d9aff720c60df8d6df89059b533
anas-ou <anas.outiskte@edu.uiz.ac.ma>
2024-02-20 13:50:19 +0100

    5. Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Inheritance Module
+
+The purpose of that module is to play around, and
+practice the Inheritance Principle in Python.
+"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
