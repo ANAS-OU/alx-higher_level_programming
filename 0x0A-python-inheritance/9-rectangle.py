3497d37dd7450e7db26e5fcb3c8ea8ffc2bf9d4b
anas-ou <anas.outiskte@edu.uiz.ac.ma>
2024-02-22 03:41:36 +0100

    update

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -5,8 +5,8 @@ Inheritance Module
 The purpose of that module is to play around, and
 practice the Inheritance Principle in Python.
 """
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """Rectangle Class"""
