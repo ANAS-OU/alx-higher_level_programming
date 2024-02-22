8cb3d1e9f96780607712c2a354b18398554ae871
anas-ou <anas.outiskte@edu.uiz.ac.ma>
2024-02-22 02:13:26 +0100

    9. Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+File Module
+
+This is just a simple module, the popuse of it
+is to be familiar with files (input/output) in python
+"""
+
+
+class Student:
+    """Student Class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns JSON representation
+        of an instance of this class"""
+        return self.__dict__
