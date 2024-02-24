0f778327a4ea58749ecf393d44ee20c0e6a2269f
anas-ou <anas.outiskte@edu.uiz.ac.ma>
2024-02-24 23:02:36 +0100

    12. Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,6 +15,21 @@ class Square(Rectangle):
         """Initialization, class constructor"""
         super().__init__(size, size, x, y, id)
 
+    def update(self, *args, **kwargs):
+        """Method for updating an instance"""
+        if len(args) > 0:
+            if len(args) >= 2:
+                args = [*args[:2], *args[1:]]
+            keys = list(self.__dict__.keys())
+            idx = 0
+            for arg in args:
+                key = keys[idx]
+                self.__dict__[key] = arg
+                idx += 1
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
     def __str__(self):
         """String representation of a square"""
         return "[Square] ({}) {}/{} - {}".format(self.id,
